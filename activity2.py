59609a88b70aa7358f7a4e43985a246475e17edf
roytheboyyy <roy.mou.safa@gmail.com>
2025-06-30 19:41:47 +0300

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,5 @@
+n1=int(input("number:"))
+n2=int(input("Bottom number:"))
+if n1 % n2 == 0:
+    print(str(n1) + "is divisible by" +str(n2))
+else:print(str(n1) + "is not divisible by" + str(n2))
---
+++ activity3.py
@@ -0,0 +1,16 @@
+mean1=38
+wrongnumber=36
+correctnumber=56
+total=40
+
+
+
+sum = mean1*total
+print("the sum of 40 Nums." , sum)
+
+
+SUM2=sum-(wrongnumber) + (correctnumber)
+print("COORECT SUM" , SUM2)
+
+mean2=SUM2/total
+print(mean2)
