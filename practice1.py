533c82436a5067c42be8b685fca5fc8dc82daee8
roytheboyyy <roy.mou.safa@gmail.com>
2025-08-31 17:52:19 +0300

    Add files via upload

--- practice1.py
+++ practice1.py
@@ -1,5 +1,5 @@
-num="hi"
-if num.isascii():
-    print("yes")
-else:
-    print("no")
+# I accidentally made this forgetting my topic. Hopefully this is good!.
+i = int(input("Enter number, And i will calculate the Ammount of digits in it."))
+for n in range(i):
+    print(i-n)
+print("0")
